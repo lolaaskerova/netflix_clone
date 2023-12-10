9c336cd79dc31efe52ebf977ab9c2180ff12fb79
lolaaskerova <lolaaskrova2@gmail.com>
2023-12-10 12:06:44 +0400

    some packages and folders deleted

--- app/page.jsx
+++ app/page.jsx
@@ -6,19 +6,7 @@ import { movies } from "@/data/movies";
 import MyList from "@/components/HomeSide/MyList";
 import Navbar from "@/components/Navbar";
 
-function Home({ session }) {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (!session) {
-  //     console.log("redirecting to /auth");
-  //     router.push("/auth");
-  //   } else if (session && router.pathname !== "/") {
-  //     console.log("redirecting to  /");
-  //     router.push("/");
-  //   }
-  // }, [session, router]);
-
+function Home() {
   return (
     <div>
       <Navbar />
