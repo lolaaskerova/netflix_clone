3cc720dea6e5427dc58a6a67056cb26ac8352074
lolaaskerova <lolaaskrova2@gmail.com>
2024-02-11 20:37:07 +0400

    billboard image changed

--- components/HomeSide/Billboard.jsx
+++ components/HomeSide/Billboard.jsx
@@ -9,23 +9,22 @@ const Billboard = ({ id }) => {
   const movie = fetchData(
     `https://api.themoviedb.org/3/movie/${id}?api_key=82976aa557a864bbd01c50abfe785ff8&append_to_response=videos`
   );
-  console.log(movie);
 
   return (
     <>
       {movie.data && (
         <div
           style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.data.backdrop_path})`,
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.data.backdrop_path})`,
           }}
-          className="cursor-pointer relative lg:h-[40vw] md:h-[50vw] h-[80vw] bg-no-repeat bg-cover flex items-center"
+          className="cursor-pointer relative lg:h-[40vw] md:h-[50vw] h-[80vw] bg-no-repeat bg-cover bg-center flex items-center"
         >
           <div className="absolute top-0 bottom-0 left-0 right-0 bg-black bg-opacity-80 z-10">
             <div className=" absolute lg:top-[30%] top-[30%] left-5 py-5 ">
               <h1 className="lg:text-6xl text-2xl text-white mb-2">
                 {movie.data.original_title}
               </h1>
-              <p className="text-white lg:max-w-2xl max-w-md lg:text-base md:text-sm text-mini">
+              <p className="text-white lg:text-base md:text-sm text-mini">
                 {movie.data.overview}
               </p>
               <div className="flex items-center gap-3">
